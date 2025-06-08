04bde34ee618add710ffb757beffe625dd3dcf8c
3rr0r445 <TravisEDuran@gmail.com>
2025-06-08 17:19:59 -0400

    Add game items and introduction to the Zombie Survival Game

--- main.py
+++ main.py
@@ -23,4 +23,50 @@ class Item:
         self.name = name
         self.description = description
         self.is_movable = is_movable
-        
+
+# Define game items here
+flashlight = Item('A Flashlight', "It's a brand new flashlight, batteries included.", True)
+
+# Define Rooms here and place items in it
+living_room = Room("The Living Room", "It's been the same since you were a child. Calming except for the lack of electricity.", )
+living_room.items.append(flashlight)
+
+# Create the player and assign starting point
+player = Player("The Player", living_room)
+
+# Create Game Introduction
+def intro():
+    print("Welcome to Travis' Zombie Survival Game!")
+    print("Created by: Travis Duran")
+    print("Special Thanks to: CodeinPlace and Stanford.")
+    print("Also a huge thanks to my fellow students in my section!")
+    print()
+    input("Press Enter to Begin...")
+    print("\n--------------------------------")
+    time.sleep(1)
+    print("The outbreak has begun. The news fell silent hours ago.")
+    time.sleep(1)
+    print("You called for help before the phones went down and have been waiting since.")
+    time.sleep(1)
+    print("You smell smoke and something else you can't quite place, something sweet and sickly.")
+    time.sleep(1)
+    print("Gunfire, screams—once constant. Now gone. The world outside is eerily still.")
+    time.sleep(1)
+    print("What you can vaguely hear from outside is the sound of shambling footsteps and moaning.")
+    time.sleep(1)
+    print("Escape is probably a pipedream at this point. You must collect resources in this house and get to the basement to await rescue.")
+    time.sleep(1)
+    print()
+
+
+# Start the Initial Game Loop
+
+#intro()
+print(f"\n{player.location.name}")
+print(player.location.description)
+print("\nExits:")
+for exit in player.location.exits:
+    print(exit)
+
+
+
